69c36e3022627eb4880e7b85d38cfef00e5d01c0
dfromerog91 <dfromerog@outlook.com>
2024-07-31 06:27:51 -0500

    V0.18

--- js/sliderScript.js
+++ js/sliderScript.js
@@ -27,8 +27,33 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 
     // Prueba para inicializar (Probablemente esto vaya en el onLoad)
-    initializeDial('AsleepOTABackOffDial1', 'AsleepOTABackOffDialValue1', 0, 60, '150px', 15);
-    initializeDial('SleepBackoffTimerDial1', 'SleepBackoffTimerDialValue1', 0, 60, '150px', 15);
-    initializeDial('dial1', 'dialValue1', 0, 60, '150px', 15);
-    initializeDial('dial2', 'dialValue2', 10, 60, '100px', 12);
+    initializeDial('DMS_DriverAsleep_MessageBackoff_dial_test', 'DMS_DriverAsleep_MessageBackoff_value_test', 0, 60, '150px', 10);
+    initializeDial('DMS_DriverAsleep_FeedbackBackoff_dial_test', 'DMS_DriverAsleep_FeedbackBackoff_value_test', 0, 60, '150px', 10);
+    initializeDial('DMS_DriverAsleep_SpeedThreshold_dial_test', 'DMS_DriverAsleep_SpeedThreshold_value_test', 0, 60, '150px', 10);
+
+    initializeDial('DMS_DriverDrowsiness_MessageBackoff_dial_test', 'DMS_DriverDrowsiness_MessageBackoff_value_test', 0, 60, '150px', 15);
+    initializeDial('DMS_DriverDrowsiness_FeedbackBackoff_dial_test', 'DMS_DriverDrowsiness_FeedbackBackoff_value_test', 0, 60, '150px', 15);
+    initializeDial('DMS_DriverDrowsiness_SpeedThreshold_dial_test', 'DMS_DriverDrowsiness_SpeedThreshold_value_test', 0, 60, '150px', 15);
+
+    initializeDial('DMS_DriverDistracted_MessageBackoff_dial_test', 'DMS_DriverDistracted_MessageBackoff_value_test', 0, 60, '150px', 15);
+    initializeDial('DMS_DriverDistracted_FeedbackBackoff_dial_test', 'DMS_DriverDistracted_FeedbackBackoff_value_test', 0, 60, '150px', 15);
+    initializeDial('DMS_DriverDistracted_SpeedThreshold_dial_test', 'DMS_DriverDistracted_SpeedThreshold_value_test', 0, 60, '150px', 15);
+
+    initializeDial('DMS_PhoneUse_MessageBackoff_dial_test', 'DMS_PhoneUse_MessageBackoff_value_test', 0, 60, '150px', 15);
+    initializeDial('DMS_PhoneUse_FeedbackBackoff_dial_test', 'DMS_PhoneUse_FeedbackBackoff_value_test', 0, 60, '150px', 15);
+    initializeDial('DMS_PhoneUse_SpeedThreshold_dial_test', 'DMS_PhoneUse_SpeedThreshold_value_test', 0, 60, '150px', 15);
+
+    initializeDial('DMS_Seatbelt_MessageBackoff_dial_test', 'DMS_Seatbelt_MessageBackoff_value_test', 0, 60, '150px', 15);
+    initializeDial('DMS_Seatbelt_FeedbackBackoff_dial_test', 'DMS_Seatbelt_FeedbackBackoff_value_test', 0, 60, '150px', 15);
+    initializeDial('DMS_Seatbelt_SpeedThreshold_dial_test', 'DMS_Seatbelt_SpeedThreshold_value_test', 0, 60, '150px', 15);
+
+    initializeDial('DMS_Smoking_MessageBackoff_dial_test', 'DMS_Smoking_MessageBackoff_value_test', 0, 60, '150px', 15);
+    initializeDial('DMS_Smoking_FeedbackBackoff_dial_test', 'DMS_Smoking_FeedbackBackoff_value_test', 0, 60, '150px', 15);
+    initializeDial('DMS_Smoking_SpeedThreshold_dial_test', 'DMS_Smoking_SpeedThreshold_value_test', 0, 60, '150px', 15);
+
+    initializeDial('ADAS_LaneDeparture_MessageBackoff_dial_test', 'ADAS_LaneDeparture_MessageBackoff_value_test', 0, 60, '150px', 15);
+
+    initializeDial('ADAS_Tailgating_MessageBackoff_dial_test', 'ADAS_Tailgating_MessageBackoff_value_test', 0, 60, '150px', 15);
+
+    initializeDial('ADAS_UFCW_MessageBackoff_dial_test', 'ADAS_UFCW_MessageBackoff_value_test', 0, 60, '150px', 15);
 });
